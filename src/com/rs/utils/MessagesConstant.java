5fe8ca2e8b32bf24b1d3d43a97ec22d50f6eda8c
kennethrsps <37264714+kennethrsps@users.noreply.github.com>
2025-06-29 01:03:43 +0800

    Delete src directory

--- src/com/rs/utils/MessagesConstant.java
+++
@@ -1,6 +0,0 @@
-package com.rs.utils;
-
-public class MessagesConstant {
-
-    public static String MYSQL_DISABLED = "Mysql connections are disabled. If you have any questions regarding this contact an administrator.";
-}
